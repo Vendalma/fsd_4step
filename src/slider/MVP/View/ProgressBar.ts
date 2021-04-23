b685dc7a6d4435d28aa7d994fbaf51076d6b4c18
Olga Maurer <olga.maurer898@gmail.com>
2021-04-23 09:42:29 +0300

    #5 Исправлены имена файлов

---
+++ src/slider/MVP/View/ProgressBar.ts
@@ -0,0 +1,68 @@
+import { IPositionState } from './types';
+
+interface IConfigBar {
+  range: boolean;
+  vertical: boolean;
+}
+
+class ProgressBar {
+  private config: IConfigBar;
+
+  private slider: HTMLElement;
+
+  private progressBar: HTMLElement;
+
+  constructor(slider: HTMLElement) {
+    this.slider = slider;
+    this.createBar();
+  }
+
+  addBar(values: IPositionState): void {
+    const positionThumbFirst = values.positionFrom.position;
+    const positionThumbSecond = values.positionTo.position;
+    if (!this.config.vertical) {
+      if (!this.config.range) {
+        this.progressBar.style.left = '0px';
+        this.progressBar.style.width = `${positionThumbFirst}px`;
+      } else {
+        this.progressBar.style.left = `${positionThumbFirst}px`;
+        this.progressBar.style.width = `${positionThumbSecond - positionThumbFirst}px`;
+      }
+    }
+
+    if (this.config.vertical) {
+      if (!this.config.range) {
+        this.progressBar.style.top = '0px';
+        this.progressBar.style.height = `${positionThumbFirst}px`;
+      } else {
+        this.progressBar.style.top = `${positionThumbFirst}px`;
+        this.progressBar.style.height = `${positionThumbSecond - positionThumbFirst}px`;
+      }
+    }
+  }
+
+  updateConfig(data: IConfigBar): void {
+    this.config = data;
+    this.progressBar.removeAttribute('style');
+    this.checkOrientation();
+  }
+
+  private createBar(): void {
+    this.progressBar = document.createElement('div');
+    this.progressBar.classList.add('slider__progress-bar');
+    this.progressBar.classList.add('js-slider__progress-bar');
+    this.slider.prepend(this.progressBar);
+  }
+
+  private checkOrientation(): void {
+    if (!this.config.vertical) {
+      this.progressBar.classList.remove('slider__progress-bar_vertical');
+      this.progressBar.classList.add('slider__progress-bar_horizontal');
+    }
+    if (this.config.vertical) {
+      this.progressBar.classList.add('slider__progress-bar_vertical');
+      this.progressBar.classList.remove('slider__progress-bar_horizontal');
+    }
+  }
+}
+export default ProgressBar;
---
+++ src/slider/RangeSlider/RangeSlider.ts
@@ -0,0 +1,55 @@
+import Model from '../MVP/Model/Model';
+import Presenter from '../MVP/Presenter/Presenter';
+import View from '../MVP/View/View';
+import { IConfig, IUpdatedConfig, IUpdatedPosition } from './types';
+
+class RangeSlider {
+  private model: Model;
+
+  private presenter: Presenter;
+
+  private view: View;
+
+  private settings: IConfig;
+
+  private container: HTMLElement;
+
+  constructor(container: HTMLElement, settings: IConfig) {
+    this.settings = settings;
+    this.container = container;
+    this.init();
+    this.setConfig(settings);
+  }
+
+  setConfig(data: IUpdatedConfig | IConfig): void {
+    this.model.updateConfig(Object.assign(this.settings, data));
+  }
+
+  getConfig(): IConfig {
+    this.model.subscribe((data) => {
+      if (data.type === 'configChanged') {
+        this.settings = data.value;
+      }
+    });
+    return this.settings;
+  }
+
+  getPosition(fn: (value?: IUpdatedPosition) => void): void {
+    this.model.subscribe((data) => {
+      if (data.type === 'configChanged') {
+        fn({
+          positionFrom: data.value.positionFrom,
+          positionTo: data.value.positionTo,
+        });
+      }
+    });
+  }
+
+  private init() {
+    this.model = new Model();
+    this.view = new View(this.container);
+    this.presenter = new Presenter(this.model, this.view);
+  }
+}
+
+export default RangeSlider;
